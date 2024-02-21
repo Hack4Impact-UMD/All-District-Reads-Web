9bff9b1c8afb7e745af91642664c441a4a6b1431
wavydang7 <davy1112@terpmail.umd.edu>
2024-02-20 23:54:35 -0500

    Davy

--- client/my-app/src/Pages/Login.tsx
+++ client/my-app/src/Pages/Login.tsx
@@ -1,1 +1,33 @@
-import React from 'react'; 
+import React, {useState} from 'react';
+
+import '../Login.css'
+
+const Login = () => {
+    return (
+        <div className={'mainContainer'}>
+      <div className={'titleContainer'}>
+        <div>All District Reads Login</div>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          placeholder="Enter your email here"
+          className={'inputBox'}
+        />
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          placeholder="Enter your password here"
+          className={'inputBox'}
+        />
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input className={'inputButton'} type="button" value={'Log in'} />
+      </div>
+    </div>
+    )
+}
+
+export default Login;
