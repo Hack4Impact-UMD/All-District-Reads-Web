ffb00a92d2764856a64e42f3107e13a17709ad81
lmuratore <ltm157m@gmail.com>
2024-04-02 15:46:19 -0400

    Added import

--- client/my-app/src/Components/ReadingSchedule/ReadingSchedule.tsx
+++ client/my-app/src/Components/ReadingSchedule/ReadingSchedule.tsx
@@ -12,6 +12,7 @@ import {
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { Dayjs } from 'dayjs';
 
 interface ReadingScheduleProps {}
 
