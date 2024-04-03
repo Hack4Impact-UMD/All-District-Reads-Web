4805de5c21564fcd7e9a4ebd6f7e0c0b24e4c052
harthir <138251519+harthir@users.noreply.github.com>
2024-04-03 00:09:30 -0400

    Merge Conflict

--- client/my-app/src/index.tsx
+++ client/my-app/src/index.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from './App';
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Library from "./Pages/Library";
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
+
 
 
 const root = ReactDOM.createRoot(
