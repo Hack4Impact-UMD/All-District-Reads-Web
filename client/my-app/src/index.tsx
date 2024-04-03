9213cc03a8405c660119cbd7e4507811f36542fb
harthir <138251519+harthir@users.noreply.github.com>
2024-04-02 23:53:09 -0400

    Merge Conflicts

--- client/my-app/src/index.tsx
+++ client/my-app/src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Login from './Pages/Login';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Library from "./Pages/Library";
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
 
 
 const root = ReactDOM.createRoot(
@@ -12,7 +15,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <App /> */}
+    {/*<App />*/}
+    <Library />
  
   </React.StrictMode>
 );
