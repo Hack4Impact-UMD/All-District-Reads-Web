b3aad711a143e08572603ed59c9bb558aef2129b
harthir <138251519+harthir@users.noreply.github.com>
2024-04-03 21:36:44 -0400

    Merge Conflict

--- client/my-app/src/index.tsx
+++ client/my-app/src/index.tsx
@@ -4,9 +4,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Library from "./Pages/Library";
+
 import Login from './Pages/Login';
-import Dashboard from './Pages/Dashboard';
+
 
 
 
@@ -17,7 +17,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/*<App />*/}
-    <Library />
+   
   </React.StrictMode>
 );
 
