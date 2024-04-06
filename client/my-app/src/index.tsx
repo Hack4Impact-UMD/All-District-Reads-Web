2f25831c3474f235725065baf84fe057811b4858
harthir <138251519+harthir@users.noreply.github.com>
2024-04-06 12:20:27 -0400

    Merge Conflicts

--- client/my-app/src/index.tsx
+++ client/my-app/src/index.tsx
@@ -1,23 +1,20 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import Login from './Pages/Login';
-import reportWebVitals from './reportWebVitals';
-import ReadingSchedule from './ReadingSchedule';
-
+import ReadingSchedule from './ReadingSchedule'; // Include the ReadingSchedule component
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-
     {/* <App /> */}
-    <ReadingSchedule />
+    <ReadingSchedule /> {/* Use ReadingSchedule component */}
   </React.StrictMode>
-
 );
 
 reportWebVitals();
