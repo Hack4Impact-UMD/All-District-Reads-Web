c90e3b1a1253dcc34287f167531a4e0272aa8104
Psedghi <76452130+Psedghi@users.noreply.github.com>
2024-04-09 16:20:12 -0400

    added animation changes for the home page.

--- client/my-app/src/Pages/Home.tsx
+++ client/my-app/src/Pages/Home.tsx
@@ -4,27 +4,30 @@ import "../Home.css";
 const Home = () => {
     return (
         <div className="home-container">
-            <header className="home-header">
-                <h1>Welcome Back</h1>
-                <nav className="home-nav">
+            <div className="top-half">
+                <header className="home-header">
+                    <h1>Welcome Back</h1>
+                    <nav className="home-nav">
 
-                </nav>
-            </header>
-
-            <main className="home-main">
-                <section className="home-card">
-                    <h2>Library of Books</h2>
-                    <div className="home-icon"></div>
-                </section>
-                <section className="home-card">
-                    <h2>Reading Schedule</h2>
-                    <div className="home-icon"></div>
-                </section>
-                <section className="home-card">
-                    <h2>Dashboard</h2>
-                    <div className="home-icon"></div>
-                </section>
-            </main>
+                    </nav>
+                </header>
+            </div>
+            <div className="bottom-half">
+                <main className="home-main">
+                    <section className="home-card">
+                        <h2>Library of Books</h2>
+                        <div className="home-icon"></div>
+                    </section>
+                    <section className="home-card">
+                        <h2>Reading Schedule</h2>
+                        <div className="home-icon"></div>
+                    </section>
+                    <section className="home-card">
+                        <h2>Dashboard</h2>
+                        <div className="home-icon"></div>
+                    </section>
+                </main>
+            </div>
         </div>
     );
 };
