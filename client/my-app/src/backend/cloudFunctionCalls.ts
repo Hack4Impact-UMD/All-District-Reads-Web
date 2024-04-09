732d261d9d7f9d9a2180b79b648ab61bb5a6fcc8
lmuratore <ltm157m@gmail.com>
2024-04-09 11:27:05 -0400

    Added cloud function callers for ADR Staff and SchoolStaff

--- client/my-app/src/backend/cloudFunctionCalls.ts
+++ client/my-app/src/backend/cloudFunctionCalls.ts
@@ -1,5 +1,3 @@
-import {initializeApp} from 'firebase/app';
-import {getAuth, sendPasswordResetEmail} from '@firebase/auth';
 import {getFunctions, httpsCallable} from 'firebase/functions';
 
 export function createAdminUser(
@@ -15,13 +13,64 @@ export function createAdminUser(
       schoolId: "",
       schoolDistrictId: "",
       numChildren: "",
+      userType: "ADRAdmin"
     })
       .then(async () => {
         resolve();
       })
       .catch(error => {
         console.log(error.message);
-        reject();
+        reject(error);
+      });
+  });
+}
+
+export function createADRStaffUser(
+  email: string
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const functions = getFunctions();
+    const createADRStaffCloudFunction = httpsCallable(functions, "createADRStaffUser");
+
+    createADRStaffCloudFunction({
+      email: email,
+      name: "test",
+      numChildren: 2,
+      schoolId: "",
+      schoolDistrictId: "",
+      userType: "ADRStaff",
+    })
+      .then(async () => {
+        resolve();
+      })
+      .catch(error => {
+        console.log(error.message);
+        reject(error);
+      });
+  });
+}
+
+export function createSchoolStaffUser(
+  email: string
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const functions = getFunctions();
+    const createADRStaffCloudFunction = httpsCallable(functions, "createSchoolStaffUser");
+
+    createADRStaffCloudFunction({
+      email: email,
+      name: "test",
+      numChildren: 2,
+      schoolId: "",
+      schoolDistrictId: "",
+      userType: "SchoolStaff",
+    })
+      .then(async () => {
+        resolve();
+      })
+      .catch(error => {
+        console.log(error.message);
+        reject(error);
       });
   });
 }
