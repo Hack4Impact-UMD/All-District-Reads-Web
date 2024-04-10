d7c91c79d7d7bedeff1e4613a850f0814f9e5ceb
Psedghi <76452130+Psedghi@users.noreply.github.com>
2024-04-10 11:41:45 -0400

    added the routing

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,47 +1,85 @@
-import "./App.css";
+// import "./App.css";
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Login from './Pages/Login';
+// import Schedule from './Pages/Schedule';
+// import { FirebaseOptions, initializeApp } from "firebase/app";
+// import firebaseConfig from './config/firebase';
+// import Library from "./Pages/Library";
+// import Dashboard from './Pages/Dashboard';
+// import Home from "./Pages/Home"
+
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+
+//       <header className="App-header">Hello world!</header>
+
+//       <Login />
+//     </div>
+//   );
+// }
+
+// const Apps = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">Hello world!</header>
+//     </div>
+//   );
+// }
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig as FirebaseOptions);
+
+// const AppF = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         Hello world!
+//       </header>
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// export default App;
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './Pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Dashboard from './Pages/Dashboard';
+import Library from "./Pages/Library";
 import Schedule from './Pages/Schedule';
-import { FirebaseOptions, initializeApp } from "firebase/app";
+import Login from './Pages/Login';
+import { FirebaseOptions, initializeApp, getApps } from "firebase/app";
 import firebaseConfig from './config/firebase';
+
+// Initialize Firebase only if there isn't an instance already
+if (!getApps().length) {
+  initializeApp(firebaseConfig as FirebaseOptions);
+}
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      
-      <header className="App-header">Hello world!</header>
-
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          Hello world!
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-const Apps = () => {
-  return (
-    <div className="App">
-      <header className="App-header">Hello world!</header>
-    </div>
-  );
-}
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig as FirebaseOptions);
-
-const AppF = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello world!
-      </header>
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
--- client/my-app/src/index.tsx
+++ client/my-app/src/index.tsx
@@ -1,24 +1,46 @@
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import Library from "./Pages/Library";
+// import Login from './Pages/Login';
+// import Dashboard from './Pages/Dashboard';
+// import Home from "./Pages/Home";
+
+
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement,
+// );
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     {/*<App />*/}
+//     <Home />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Library from "./Pages/Library";
-import Login from './Pages/Login';
-import Dashboard from './Pages/Dashboard';
-import Home from "./Pages/Home"
 
-
+// Find the root of your app
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
+
+// Render the App component instead of Home
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/*<App />*/}
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
+// Call the function to report web vitals
 reportWebVitals();
