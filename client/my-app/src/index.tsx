55ae0f2b50c4929ea9ad5127f809f74e28a9e044
wavydavy7 <davy1112@terpmail.umd.edu>
2024-04-24 21:44:12 -0400

    no longer double

--- client/my-app/src/index.tsx
+++ client/my-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -12,7 +12,6 @@ const root = ReactDOM.createRoot(
 // Render the App component instead of Home
 root.render(
   <React.StrictMode>
-    <App />
     <App />
     {/* <Library /> */}
   </React.StrictMode>
